cd820d479dd4df4cd82ee771c8f22e7b7104c759
Matheus <matheusvclls97@gmail.com>
2022-12-23 10:19:51 -0300

    feat: add get_activities function

--- src/infra/strava_api_consumer.py
+++ src/infra/strava_api_consumer.py
@@ -40,6 +40,38 @@ class StravaApiConsumer():
                 status_code=status_code, request=req, response=response.json()
             )
 
+
+    def get_activities(self, page: int) -> Tuple[int, Type[Request], Dict]:
+        '''
+        Request activities by page    
+        Args:
+            None
+        Returns
+            Tuple with status_code, request, response attributes
+        ''' 
+        req = requests.Request(
+            method='GET',
+            url="https://www.strava.com/api/v3/athlete/activities",
+            headers={
+            "Authorization": f"Bearer {token}",
+            },
+            params={"page":page}
+
+        )
+        req_prepared = req.prepare()
+
+        response = self.__send_http_request(req_prepared)
+        status_code = response.status_code
+
+        if ((status_code >= 200) and (status_code <= 299) and (len(response.json())>0)):
+            return self.get_athlete_info_response(
+                status_code=status_code, request=req, response=response.json()
+            )
+        if ((status_code >= 200) and (status_code <= 299) and (len(response.json())==0)):
+            return self.get_athlete_info_response(
+                status_code=status_code, request=req, response=response.json()
+            )
+
     @classmethod
     def __send_http_request(cls, req_prepared: Type[Request]) -> any:
         '''
