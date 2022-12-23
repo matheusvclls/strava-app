649c631de32b2a866f66668cdeca8e0b25f62870
Matheus <matheusvclls97@gmail.com>
2022-12-23 10:20:27 -0300

    test: add tests to the get_activities function

--- src/infra/strava_api_consumer_test.py
+++ src/infra/strava_api_consumer_test.py
@@ -1,14 +1,27 @@
 from .strava_api_consumer import StravaApiConsumer
 
-def test_get_athlete_info():
+def test_get_activities():
     ''' Test get_athlete_info method '''
 
     strava_api_consumer = StravaApiConsumer()
+    page=1
+    get_activities_response = strava_api_consumer.get_activities(page=page)
 
-    get_athlete_info_response = strava_api_consumer.get_athlete_info()
+    assert get_activities_response.request.method == 'GET'
+    assert get_activities_response.request.url == 'https://www.strava.com/api/v3/athlete/activities'
 
-    assert get_athlete_info_response.request.method == 'GET'
-    assert get_athlete_info_response.request.url == 'https://www.strava.com/api/v3/athlete'
+    assert get_activities_response.status_code == 200
+    assert len(get_activities_response.response) > 0
+    assert isinstance(get_activities_response.response, list)
 
-    assert get_athlete_info_response.status_code == 200
-    assert isinstance(get_athlete_info_response.response, dict)
+
+def test_get_activities_empty_list():
+    ''' Test get_activities method when returns an empty list'''
+
+    strava_api_consumer=StravaApiConsumer()
+    page=200000
+
+    get_activities_response = strava_api_consumer.get_activities(page=page)
+
+    assert get_activities_response.response == []
+        
