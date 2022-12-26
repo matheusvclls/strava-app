b3e0eec6d4e1bf2be2e8ef6b5a2eb748e8ce5cbd
Matheus <matheusvclls97@gmail.com>
2022-12-26 10:52:54 -0300

    feat: add script to run fastapi framework testing it

---
+++ run.py
@@ -0,0 +1,11 @@
+import uvicorn
+
+print('hello world')
+
+if __name__=="__main__":
+    uvicorn.run(
+        "src.main.config.http_server_configs:app",
+        host='0.0.0.0',
+        port=8000,
+        reload=True
+    )
