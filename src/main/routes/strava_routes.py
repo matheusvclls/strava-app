83dca44a325b351d8b2b2c5fe255a14e20c49c7c
Matheus <matheusvclls97@gmail.com>
2022-12-28 21:49:19 -0300

    test: implement the first route using fast api

--- src/main/routes/strava_routes.py
+++ src/main/routes/strava_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request as RequestFastApi
 
 strava_routes = APIRouter()
 
-@strava_routes.get('/api/strava/list')
-def get_running_in_pagination(request: RequestFastApi):
+@strava_routes.get('/api/strava/test')
+def get_test_fast_api(request: RequestFastApi):
     print(request.query_params)
-    return {'hello': 'world!', 'params':request.query_params}
+    return {'hello': 'world!'}
