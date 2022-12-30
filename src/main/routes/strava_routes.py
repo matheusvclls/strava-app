32fe9ed90ede40bab944a52a17cd3e31be9a4a18
Matheus <matheusvclls97@gmail.com>
2022-12-29 21:21:20 -0300

    feat: add list as get method to the strava route

--- src/main/routes/strava_routes.py
+++ src/main/routes/strava_routes.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter, Request as RequestFastApi
+from fastapi.responses import JSONResponse
+
+from src.validators.get_running_collector_validator import get_pagination_validator
+
+from src.main.adapters import request_adapter
+from src.main.composer.get_running_collector_composer import get_running_collector_composer
+
+from src.presenters.errors.error_controller import handle_errors
 
 strava_routes = APIRouter()
 
@@ -6,3 +14,21 @@ strava_routes = APIRouter()
 def get_test_fast_api(request: RequestFastApi):
     print(request.query_params)
     return {'hello': 'world!'}
+
+@strava_routes.get("/api/strava/list")
+async def get_running_collector_list(request: RequestFastApi):
+
+    response = None
+    controller = get_running_collector_composer()
+    
+    try:
+        get_pagination_validator(request)
+        response = await request_adapter(request, controller.handler)
+
+    except Exception as e:
+        response = handle_errors(e)
+    
+    return JSONResponse(
+        status_code=response["status_code"],
+        content=response["data"]
+    )
